6e9ec884d3d933d6eff38611ca87c33bf947c52d
rodolfoSantosUK <santoseuro2018@gmail.com>
2021-03-21 21:18:22 -0300

    inicio

---
+++ template/src/main/java/rc/legostore/repository/LegoSetRepository.java
@@ -0,0 +1,13 @@
+package rc.legostore.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import rc.legostore.model.LegoSet;
+
+
+@Repository
+public interface LegoSetRepository extends MongoRepository<LegoSet, String>{
+
+
+
+}
